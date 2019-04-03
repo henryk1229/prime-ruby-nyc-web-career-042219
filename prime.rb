7a8296725c19c71fa63664604ae1739ea6ed7f79
Henry Koehler <henryk1229@gmail.com>
2019-04-03 16:53:02 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
-def prime?(integer)
-  if integer <= 1  
-    false
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
   else
-     (2..integer).to_a.all? do | n |
-      integer % n != 0
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
     end
-    true 
   end
 end
   
